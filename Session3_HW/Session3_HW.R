e2e2084aa3cb6dd0a7269f0eff41f8f1ba824f7c
Tyler Abbot <tyler.abbot@sciencespo.fr>
2016-09-26 12:48:35 +0200

    Tylers comments on weeks 1-3.

--- Session3_HW/Session3_HW.R
+++ Session3_HW/Session3_HW.R
@@ -1,6 +1,12 @@
-#Week 2 - Second Assignment 
+#Week 2 - Second Assignment
 
-#I was very ill organized this week and I did not have time to do this 
-#problem set. I am sorry for this and will try to do it this week-end, 
+####### Comment
+# No worries Julie! I understood when designing the grading that this
+# course would move to the bottom of your list of things to do. Do
+# take a look at HW3, as functions and nested functions are important
+# and difficult!
+
+#I was very ill organized this week and I did not have time to do this
+#problem set. I am sorry for this and will try to do it this week-end,
 #knowing that it won't be considered as a "done" problem set :)
 #See you !
