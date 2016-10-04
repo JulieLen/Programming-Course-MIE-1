4947b64b66247debad7db3d1b39de45f61c57e0d
Julie Lenoir <julielenoir@MacBook-Air-de-Julie.local>
2016-10-04 12:17:07 +0200

    First commit_no read me

---
+++ Session5_HW/Session5_HW_Package.R
@@ -0,0 +1,9 @@
+#### Session 5 - Homework ####
+
+# Create a Package 
+
+library("devtools")
+
+setwd("~/Documents/1 - ScPo/16:18 - MIE/Programming course/Programming-Course-MIE-1/Session5_HW")
+create('JuliesPackage')
+
